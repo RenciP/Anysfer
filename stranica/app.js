e7cc388ea2090335557deec26a045dd852e01ff8
RenciP <renato.strek@gmail.com>
2022-03-14 15:40:40 +0100

    -upload radi, download ne radi

--- stranica/app.js
+++ stranica/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
+const fileUpload = require('express-fileupload')
+const bodyParser = require('body-parser')
+const passGenerator = require('generate-password')
+
 const app = express()
 
 const port = 1092
 
+app.use(fileUpload({
+    createParentPath: true
+}))
+app.use(bodyParser.json())
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', (req, res) => {
@@ -12,7 +22,25 @@ app.get('/', (req, res) => {
 })
 
 app.post('/api/upload', (req,res) => {
-    console.log(req.body)    
+    const file = req.files.file
+    const passcode = passGenerator.generate({
+        length: 7,
+        numbers: true
+    })
+    fs.mkdir(path.join(__dirname, 'uploads', passcode), (err) => {
+        if (err) {
+            return console.error(err);
+        }
+    });
+    file.mv('./uploads/' + passcode + '/' + file.name)    
+})
+
+app.get('/api/download', (req, res) => {
+    const passcode = req.passcode
+    console.log(req.body);
+    /*fs.readdir(path.join(__dirname, +'uploads' + passcode), 'utf-8',(err, files) => {
+        files.forEach((file) => console.log(file.name))
+    } )*/
 })
 
 app.listen(port, () => {
--- stranica/public/frontend.js
+++ stranica/public/frontend.js
@@ -1,15 +1,43 @@
-document.querySelector("#download-btn").addEventListener("click", function () {
-    console.log('Download clicked')
+const uploadUrl = '/api/upload'
+const downloadUrl = '/api/download'
+
+const downloadBtnDOM = document.querySelector("#download-btn")
+const fileUploadDOM = document.querySelector('.file-upload-form')
+const selectFileDOM = document.querySelector('#data-input')
+const passcodeDOM = document.querySelector('#passcode')
+
+let fileForUpload
+
+downloadBtnDOM.addEventListener("click", function () {
+    console.log(passcodeDOM.value)
+    const passcode = passcodeDOM.value
+    downloadFile(passcode)
   })
 
-document.querySelector("#upload-btn").addEventListener("click", function () {
-    uploadFile()
-  })
 
-function uploadFile() {
-  axios.post('/api/upload', {
-    imeFajla: 'Fajlovo presveto ime',
-    sadrzaj: 'sadrzajFajla'
+fileUploadDOM.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const formData = new FormData()
+
+  formData.append('file', selectFileDOM.files[0])
+
+  console.log(formData.get('file'));
+
+  uploadFile(formData)
+})
+
+function uploadFile(file) {
+  axios.post('/api/upload', file, {
+    headers: {
+      'Content-Type':'multipart/form-data'
+    }
   }).then(console.log('Fajl uploadan'))
+  .catch(function(error){console.log(error)})
+}
+
+function downloadFile(passcode){
+  console.log(passcode);
+  axios.get(downloadUrl, passcode)
+  .then(console.log(('File downloaded')))
   .catch(function(error){console.log(error)})
 }
